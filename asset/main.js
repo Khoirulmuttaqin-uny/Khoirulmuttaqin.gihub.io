9d732144f1bb1d41d7f72a497374d9cf8c91041b
Khoirulmuttaqin-uny <khoirulmuttaqin.2021@student.uny.ac.id>
2022-03-13 11:38:25 +0700

    first commit

---
+++ asset/main.js
@@ -0,0 +1,10 @@
+console.log("Test Javascript");
+alert("Selamat Datang di Website Submission Khoirul Muttaqin");
+
+//edited profile
+document.getElementById("name").innerHTML = "KHOIRUL MUTTAQIN";
+document.getElementById("date").innerHTML = "10 Oktober 2002";
+document.getElementById("age").innerHTML = "19 Tahun";
+document.getElementById("city").innerHTML = "Pati";
+document.getElementById("religion").innerHTML = "Islam";
+document.getElementById("graduate").innerHTML = "Universitas Negeri Yogyakarta";
